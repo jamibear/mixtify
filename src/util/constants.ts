0f9910b105cc38ca07a23e27932bca9786581ad5
jamibear <5701ja.mi@gmail.com>
2023-06-24 20:08:44 +0800

    update redirect uri to deployed site

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -7,5 +7,5 @@ export const SPOTIFY_CLIENT_ID = import.meta.env.SPOTIFY_CLIENT_ID ?? "";
 export const SPOTIFY_CLIENT_SECRET =
   import.meta.env.SPOTIFY_CLIENT_SECRET ?? "";
 
-export const REDIRECT_URI = "http://localhost:3000/callback";
-export const APP_URL = "https://localhost:3000/";
+export const REDIRECT_URI = "https://mixtify.netlify.app/callback";
+export const APP_URL = "https://mixtify.netlify.app/";
