cf2ce1b6542d9cec5486552c7b319d730526b5cf
jamibear <5701ja.mi@gmail.com>
2023-06-27 09:41:31 +0800

    optional chaining on items

--- src/components/TopArtist.tsx
+++ src/components/TopArtist.tsx
@@ -1,18 +1,18 @@
 import "../styles/Spotify.css";
 
 const TopArtist = ({ topArtists }) => {
-    return (
-        <div>
-            <h1 className="title">Top Artists</h1>
-            <div className="grid grid-cols-5 gap-1 max-w-2xl mx-auto p-4">
-                {topArtists.items.map((item) => (
-                    <div className="cover">
-                        <img src={item.images[0].url} />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="title">Top Artists</h1>
+      <div className="grid grid-cols-5 gap-1 max-w-2xl mx-auto p-4">
+        {topArtists.items?.map((item) => (
+          <div className="cover">
+            <img src={item.images[0].url} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TopArtist;
